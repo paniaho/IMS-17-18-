b59ed50ce883ff00917fda102359faee6245fed3
Roman Kiselevich <kiselevich2012@gmail.com>
2017-12-05 11:49:45 +0100

    Add python script and .gitignore

---
+++ distances-python/distances.py
@@ -0,0 +1,17 @@
+distances = {
+    'A': 114.17,
+    'B': 77.16,
+    'C': 73.64,
+    'D': 164.8,
+    'E': 153.0,
+    'F': 142.64,
+    'G': 163.48,
+    'H': 110
+}
+
+personMinSpeed = 1.12 # m/s
+personMaxSpeed = 1.39 # m/s
+
+if __name__ == '__main__':
+    for building, distance in distances.items():
+        print("{}: Uniform({}, {})".format(building, personMinSpeed*distance, personMaxSpeed*distance))
